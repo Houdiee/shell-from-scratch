6a2ec48c06ad2c4c424a50badff03907b685fabb
Kerim <kerimugurlu24@gmail.com>
2025-08-25 00:51:30 +1000

    print error

--- error.c
+++ error.c
@@ -1,16 +1,21 @@
+#include "colors.h"
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 
+void print_error(char *command, char *error_message) {
+  fprintf(stderr, "%s%s: %s%s\n", RED_COLOR, command, error_message, RESET);
+}
+
 void exit_on_exec_error(char *command) {
   switch (errno) {
   case EACCES:
-    fprintf(stderr, "%s: permission denied\n", command);
+    print_error(command, "permission denied");
     exit(126);
     break;
 
   case ENOENT:
-    fprintf(stderr, "%s: command not found\n", command);
+    print_error(command, "command not found");
     exit(127);
     break;
 
@@ -18,8 +23,4 @@ void exit_on_exec_error(char *command) {
     perror(command);
     exit(EXIT_FAILURE);
   }
-}
-
-void print_error(char *command, char *error_message) {
-  fprintf(stderr, "%s: %s", command, error_message);
 }
