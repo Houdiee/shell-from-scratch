755db63cd6222a7c29bf16eb6b4f783de2feeb67
Kerim <kerimugurlu24@gmail.com>
2025-08-24 14:07:08 +1000

    tokenizing

--- main.c
+++ main.c
@@ -1,17 +1,20 @@
+#include "sys/types.h"
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 const int INITIAL_BUFF_SIZE = 4096;
+const int INITIAL_MAX_ARGS_COUNT = 128;
 const int GROWTH_FACTOR = 2;
 
 char *get_user_input() {
   int current_buf_size = INITIAL_BUFF_SIZE;
   char *buffer = malloc(sizeof(char) * current_buf_size);
   if (buffer == NULL) {
-    perror("failed to allocate memory for buffer");
-    return NULL;
+    perror("failed to allocate memory for user input buffer");
+    exit(EXIT_FAILURE);
   }
 
   int i = 0;
@@ -26,8 +29,8 @@ char *get_user_input() {
       current_buf_size *= GROWTH_FACTOR;
       buffer = realloc(buffer, current_buf_size);
       if (buffer == NULL) {
-        perror("failed to reallocate memory for buffer");
-        return NULL;
+        perror("failed to reallocate memory for user input buffer");
+        exit(EXIT_FAILURE);
       }
     }
 
@@ -39,13 +42,51 @@ char *get_user_input() {
   return buffer;
 }
 
+char **tokenize_string(char *string) {
+  int current_max_tokens = INITIAL_MAX_ARGS_COUNT;
+  char **tokens = malloc(sizeof(char *) * current_max_tokens);
+  if (tokens == NULL) {
+    perror("failed to allocate memory for tokenized arguments buffer");
+    exit(EXIT_FAILURE);
+  }
+
+  int i = 0;
+  char *token = strtok(string, " ");
+  while (token != NULL) {
+    if (i >= current_max_tokens) {
+      current_max_tokens *= GROWTH_FACTOR;
+      tokens = realloc(tokens, sizeof(char *) * current_max_tokens);
+      if (tokens == NULL) {
+        perror("failed to reallocate memory for tokenized arguments buffer");
+        exit(EXIT_FAILURE);
+      }
+    }
+    tokens[i] = token;
+    token = strtok(NULL, " ");
+    i++;
+  }
+
+  tokens[i] = NULL;
+  return tokens;
+}
+
 int main(int argc, char *argv[]) {
   char *user_input = get_user_input();
   if (user_input == NULL) {
     return 1;
   }
   printf("%s", user_input);
-  free(user_input);
 
+  pid_t pid = fork();
+  if (pid == -1) {
+    perror("failed to fork child process");
+    return 1;
+  }
+
+  const char *args = strtok(user_input, " ");
+
+  execlp(args, args, '\0');
+
+  free(user_input);
   return 0;
 }
